f5bb4c489d7516f8dfc09d9bb78978df3babcd01
stoellinger <stoellinger@software.tirol>
2024-10-25 11:32:32 +0200

    test method

---
+++ TestLibrary/TestLibrary/TestLibraryClass.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLibrary;
+
+public class TestLibraryClass
+{
+    public string DoSomething()
+    {
+        var greetings = $"Hello User, it is {DateTime.Now}";
+        return greetings;
+    }
+}
